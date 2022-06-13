6d873a5537d70f38c5f25dee9966a4c740aea50a
dongk00 <dddongk00@gmail.com>
2022-06-13 14:51:07 +0900

    CoroutineTest First Commit

--- coroutinetest/Retrofit/app/src/main/java/com/monthlywriting/android/retrofittest/model/ApiDataResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.monthlywriting.android.retrofittest.model
-
-
-import com.squareup.moshi.Json
-
-data class ApiDataResponse(
-    @field:Json(name = "response")
-    val response: Response?
-)
--- coroutinetest/Retrofit/app/src/main/java/com/monthlywriting/android/retrofittest/model/Items.kt
+++
@@ -1,9 +0,0 @@
-package com.monthlywriting.android.retrofittest.model
-
-
-import com.squareup.moshi.Json
-
-data class Items(
-    @field:Json(name = "item")
-    val item: List<Item?>?
-)

b5a16a12c408d28364356bbd889da7b381365a7e
dongk00 <dddongk00@gmail.com>
2022-07-15 17:33:58 +0900

    WorkManager Project Created

---
+++ WorkManagerTest/app/src/main/java/com/dong/android/workmanagertest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dong.android.workmanagertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
